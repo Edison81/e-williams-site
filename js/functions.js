cd7b5d422c11712505d1f01ff47df70dabf2bf8b
Edison81 <edison.williams1@gmail.com>
2015-04-19 16:58:20 -0400

    Week 13 (Optimization) Commit.

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,3 @@
-
-function toggle(element) {
-  var e = document.getElementById(element);
-    if(e.style.display == "block") {
-      e.style.display = "none";
-    } else {
-      e.style.display = "block";
-    }
-};
-
 $('#name').tooltip({'trigger':'focus', 'title': "What's your name?"});
 $('#email').tooltip({'trigger':'focus', 'title': "What's your email?"});
 $('#comment').tooltip({'trigger':'focus', 'title': "What do you have to say?"});
